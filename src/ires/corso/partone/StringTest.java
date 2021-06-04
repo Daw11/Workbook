5de03c73f40777aebee889abc7ddfa28d60a5b5c
Daw11 <davidebusterna@gmail.com>
2021-06-04 17:40:19 +0200

    Add StringTest

--- src/ires/corso/partone/StringTest.java
+++ src/ires/corso/partone/StringTest.java
@@ -3,6 +3,18 @@ package ires.corso.partone;
 public class StringTest {
 
     public static void main(String[] args){
-        
+        if( args.length != 3 ){
+            System.out.println("Devi inserire 3 stringhe.");
+            return;
+        }
+
+        for( int i = 0; i < 3; i++ ){
+            String str = args[i];
+            int str_length = str.length();
+            char str_start = str.charAt( 0 );
+            char str_end = str.charAt( str_length -1 );
+            String msg = String.format("La stringa %s ha lunghezza %d comincia per %s e finisce per %s", str, str_length, str_start, str_end);
+            System.out.println( msg );
+        }
     }
 }
