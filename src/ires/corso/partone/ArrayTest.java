afe5355363a7c5c8cc1afeb2765ad9082c61d1aa
Daw11 <davidebusterna@gmail.com>
2021-06-09 16:56:02 +0200

    Add removeDuplicats

--- src/ires/corso/partone/ArrayTest.java
+++ src/ires/corso/partone/ArrayTest.java
@@ -2,7 +2,34 @@ package ires.corso.partone;
 import java.util.Scanner;
 
 public class ArrayTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        test2();
+    }
+
+    public static void test1(){
+        int[] intArray = inputArray();
+
+        System.out.println("Inserisci il valore da trovare");
+        Scanner in = new Scanner( System.in );
+        int elem = in.nextInt();
+
+        int result = elemPositionInArray( intArray, elem );
+        if( result == -1 )
+            System.out.println("Elemento non trovato");
+        else
+            System.out.printf("L'elemento selezionato ha come posizione: %d", result);
+    }
+
+    public static void test2() {
+        int[] arrayWithDuplicates = inputArray();
+        int[] arrayWithoutDuplicates = removeDuplicates(arrayWithDuplicates);
+
+        System.out.println("L'array senza i duplicati è: ");
+        for(int i = 0; i < arrayWithoutDuplicates.length; i++)
+            System.out.print( arrayWithoutDuplicates[i] + "|" );
+    }
+
+    public static int[] inputArray(){
         Scanner in = new Scanner( System.in );
         System.out.println("Inserisci un array (numeri separati da |, es 1|2|3): ");
         String str = in.nextLine();
@@ -13,14 +40,7 @@ public class ArrayTest {
             intArray[i] = Integer.parseInt( strArray[i] );
         }
 
-        System.out.println("Inserisci il valore da trovare");
-        int elem = in.nextInt();
-
-        int result = elemPositionInArray( intArray, elem );
-        if( result == -1 )
-            System.out.println("Elemento non trovato");
-        else
-            System.out.printf("L'elemento selezionato ha come posizione: %d", result);
+        return intArray;
     }
 
     public static int elemPositionInArray(int[] intArray, int elem){
@@ -30,5 +50,28 @@ public class ArrayTest {
        }
 
         return -1;
+    }
+
+    public static int[] removeDuplicates(int[] arrayWithDuplicates){
+        int arrayLength = 0;
+        for( int i = 0; i < arrayWithDuplicates.length; i++ ){
+            int pos = elemPositionInArray(arrayWithDuplicates, arrayWithDuplicates[i]);
+            if( pos == i )
+                arrayLength++;
+        }
+
+        int[] arrayWithoutDuplicates = new int[arrayLength];
+
+        int j = 0;
+        for( int i = 0; i < arrayWithDuplicates.length; i++ ){
+            int elem = arrayWithDuplicates[i];
+            int pos = elemPositionInArray(arrayWithDuplicates, elem );
+            if( pos == i ) {
+                arrayWithoutDuplicates[j] = elem;
+                j++;
+            }
+        }
+
+        return arrayWithoutDuplicates;
     }
 }
