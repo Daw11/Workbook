c0692771c62b7d99febf032a1b309fdb8f73cf89
Daw11 <davidebusterna@gmail.com>
2021-07-02 11:33:24 +0200

    Pass directly the field that you want to use for sorting

--- src/ires/corso/parttwo/todo/ToDoList.java
+++ src/ires/corso/parttwo/todo/ToDoList.java
@@ -1,10 +1,9 @@
 package ires.corso.parttwo.todo;
 
-import jdk.nashorn.internal.objects.annotations.Function;
-
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 
 // GRUPPO 1:
 public class ToDoList
@@ -16,27 +15,25 @@ public class ToDoList
     // Si appoggia a un metodo di ToDoRepository per avere una lista (= copia dei TO-DO
     // originali) dei TO-DO attualmente a sistema, cioè un ArrayList facilmente utilizzabile
 
-    private static int compareByPriority(ToDo o1, ToDo o2){ return o1.getPriority().compareTo( o2.getPriority() ); }
-    private static int compareByDate(ToDo o1, ToDo o2){ return o1.getDataConsegna().compareTo( o2.getDataConsegna() ); }
-    private static int compareByState(ToDo o1, ToDo o2){ return o1.getStato().compareTo( o2.getStato() ); }
 
-    private static void showList( String msg, Comparator<ToDo> comparator ){
+    private static void showList( String msg, Function<ToDo, Comparable> field ){
         ToDoApplication.display(msg);
-        List<ToDo> sortedList = ToDoRepository.getToDoList();1
+        List<ToDo> sortedList = ToDoRepository.getToDoList();
+        Comparator<ToDo> comparator = Comparator.comparing( field );
         sortedList.sort( comparator );
         prettyPrint( sortedList );
     }
 
     public static void viewByPriority() {
-        showList( "Lista ordinata per priorità: \n", ToDoList::compareByPriority );
+        showList( "Lista ordinata per priorità: \n", ToDo::getPriority );
     }
 
     public static void viewByDate() {
-        showList( "Lista ordinata per data: \n", ToDoList::compareByDate );
+        showList( "Lista ordinata per data: \n", ToDo::getDataConsegna );
     }
 
     public static void viewByState() {
-        showList( "Lista ordinata per stato: \n", ToDoList::compareByState );
+        showList( "Lista ordinata per stato: \n", ToDo::getStato );
     }
 
     private static void prettyPrint( List<ToDo> list ){
