692869b88d945c3c5927f67a5b9fae9e6defae4b
Daw11 <davidebusterna@gmail.com>
2021-07-02 11:12:36 +0200

    Use lambda functions in ToDoList

--- src/ires/corso/parttwo/todo/ToDoList.java
+++ src/ires/corso/parttwo/todo/ToDoList.java
@@ -1,5 +1,8 @@
 package ires.corso.parttwo.todo;
 
+import jdk.nashorn.internal.objects.annotations.Function;
+
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -13,31 +16,30 @@ public class ToDoList
     // Si appoggia a un metodo di ToDoRepository per avere una lista (= copia dei TO-DO
     // originali) dei TO-DO attualmente a sistema, cioè un ArrayList facilmente utilizzabile
 
-    private final static Comparator<ToDo> priorityComparator = (ToDo o1, ToDo o2) -> o1.getPriority().compareTo( o2.getPriority() );
-    private final static Comparator<ToDo> dateComparator = (ToDo o1, ToDo o2) -> o1.getDataConsegna().compareTo( o2.getDataConsegna() );
-    private final static Comparator<ToDo> stateComparator = (ToDo o1, ToDo o2) ->o1.getStato().compareTo( o2.getStato() );
+    private static int compareByPriority(ToDo o1, ToDo o2){ return o1.getPriority().compareTo( o2.getPriority() ); }
+    private static int compareByDate(ToDo o1, ToDo o2){ return o1.getDataConsegna().compareTo( o2.getDataConsegna() ); }
+    private static int compareByState(ToDo o1, ToDo o2){ return o1.getStato().compareTo( o2.getStato() ); }
 
-    private static void showList( String msg, Comparator comparator ){
+    private static void showList( String msg, Comparator<ToDo> comparator ){
         ToDoApplication.display(msg);
-        List<ToDo> sortedList = ToDoRepository.getToDoList();
+        List<ToDo> sortedList = ToDoRepository.getToDoList();1
         sortedList.sort( comparator );
         prettyPrint( sortedList );
     }
 
     public static void viewByPriority() {
-        showList( "Lista ordinata per priorità: \n", priorityComparator );
+        showList( "Lista ordinata per priorità: \n", ToDoList::compareByPriority );
     }
 
     public static void viewByDate() {
-        showList( "Lista ordinata per data: \n", dateComparator );
+        showList( "Lista ordinata per data: \n", ToDoList::compareByDate );
     }
 
     public static void viewByState() {
-        showList( "Lista ordinata per stato: \n", stateComparator );
+        showList( "Lista ordinata per stato: \n", ToDoList::compareByState );
     }
 
     private static void prettyPrint( List<ToDo> list ){
-        for( ToDo t : list )
-            ToDoApplication.display( t.prettyPrint() );
+        list.stream().map( ToDo::prettyPrint ).forEach( ToDoApplication::display );
     }
 }
